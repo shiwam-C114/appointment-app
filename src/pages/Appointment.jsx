90b17ff31a035a0f686993237444fac10e6dcdfd
Shiwam Tiwari <zaqqst@gmail.com>
2022-07-18 17:06:03 +0530

    delete working

--- src/pages/Appointment.jsx
+++ src/pages/Appointment.jsx
@@ -1,12 +1,45 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
+import { Box, Button, Center } from "@chakra-ui/react";
+import React, { useContext, useEffect, useState } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
+import { Routes, Route, useParams } from "react-router-dom";
+import AuthContext from "../context/AuthContext";
 
 function Appointment() {
+  let { id } = useParams();
+  const [data, setData] = useState({});
+  const [isAuth, toggleAuth] = useContext(AuthContext);
+  const nav = useNavigate();
+  function getData() {
+    fetch(`http://localhost:8080/appointment/${id}`)
+      .then((res) => res.json())
+      .then(setData);
+  }
+
+  useEffect(() => {
+    getData();
+  }, []);
   return (
-    <div>Appointment
-        <Outlet/>
+    <div>
+      {isAuth ? (
+        <>
+          <Center>
+            <Box padding={"10px"} border="1px" borderColor="gray.800">
+              Name: {data.name} <br />
+              Age: {data.age} <br />
+              appointment details: {data.appointment_details} <br />
+              gender: {data.gender} <br />
+              data: {data.data} <br />
+              time: {data.time} <br />
+              <Button colorScheme="teal">Delete</Button>
+            </Box>
+          </Center>
+        </>
+      ) : (
+        nav("/login")
+      )}
+      <Outlet />
     </div>
-  )
+  );
 }
 
-export default Appointment
+export default Appointment;
--- src/pages/Appointments.jsx
+++ src/pages/Appointments.jsx
@@ -1,34 +1,50 @@
 import React, { useContext, useEffect, useState } from "react";
 import { Box, Button, Grid, GridItem, Spacer } from "@chakra-ui/react";
+import { useNavigate } from "react-router-dom";
 import AuthContext from "../context/AuthContext";
 import Login from "./Login";
 function Appointments() {
   const [data, setData] = useState([]);
   const [isAuth, toggleAuth] = useContext(AuthContext);
-  function getData() {
-    fetch("http://localhost:8080/appointment")
-      .then((res) => res.json())
-      .then(setData);
-  }
-  useEffect(() => {
-    getData();
-  }, []);
+  const nav = useNavigate()
+    function getData() {
+      fetch("http://localhost:8080/appointment")
+        .then((res) => res.json())
+        .then(setData);
+    }
+    function handleShowMore(id) {
+      nav(`/appointment/${id}`)
+    }
+
+    function handleDelete(id) {
+      fetch(`http://localhost:8080/appointment/${id}`,{
+        method: 'DELETE', headers: {
+          'Content-Type': 'application/json'
+        }
+    }).then(res=>res.json()).then(d=>{
+      console.log(d)
+      getData()
+    })
+    }
+    useEffect(() => {
+      getData();
+    }, []);
 
   return (
     <div>{isAuth?
       <Grid margin={"10px"} templateColumns="repeat(3, 1fr)" gap={6}>
         {data.map((ele) => (
           <GridItem key={ele.id}>
-            <Box border="1px" borderColor="gray.800">
+            <Box padding={"10px"} border="1px" borderColor="gray.800">
               Name: {ele.name} <br />
               Age: {ele.age} <br />
               appointment details: {ele.appointment_details} <br />
               gender: {ele.gender} <br />
               data: {ele.data} <br />
               time: {ele.time} <br />
-              <Button> show more</Button>
+              <Button onClick={()=>{handleShowMore(ele.id)}} colorScheme='teal'> show more details</Button>
               &#160;
-              <Button>Delete</Button>
+              <Button onClick={()=>{handleDelete(ele.id)}} colorScheme='teal'>Delete</Button>
             </Box>
           </GridItem>
         ))}
